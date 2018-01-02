e18b84b228f598ae13e7ed54b1673a7a75d1facd
Neeraj Wadhwa <Neeraj_Wadhwa@Zalaks-MacBook-Pro.local>
2018-01-02 15:30:05 +0530

    rename databse to database and ignore .idea dir

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
-const Chat = require('./databse/model');
+const Chat = require('./database/model');
 
 server.listen(8000);
 
